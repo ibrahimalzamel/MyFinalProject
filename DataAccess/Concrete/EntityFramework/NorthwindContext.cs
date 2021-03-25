4bb9536b8c270b83a51a0da621e38b74656736e6
İBRAHİM ELZAMİL <ibooalzam@gmail.com>
2021-03-25 16:09:10 +0300

    25/3

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -12,7 +12,6 @@ namespace DataAccess.Concrete.EntityFramework
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
-            
         }
 
         public DbSet<Product> Products { get; set; }
