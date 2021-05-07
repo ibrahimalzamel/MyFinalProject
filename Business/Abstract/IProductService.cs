c5b40a80056b299108ef416a5ffd117826d2e459
İBRAHİM ELZAMİL <ibooalzam@gmail.com>
2021-05-07 22:08:25 +0300

    07/05 commit 4

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -11,13 +11,16 @@ namespace Business.Abstract
     public interface IProductService
     {
         //IDataResult
-        IDataResult <List<Product>> GetAll();
-        IDataResult<List<Product>> GetAllByCategoryId(int Id);
-        IDataResult<List<Product>> GetByUnitPrice(decimal min , decimal max);
-        IDataResult<List< ProductDetailDto>>GetProductDetailDtos();
-        IDataResult<Product> GetById(int productId);
         IResult Add(Product product);
         IResult Update(Product product);
         IResult Delete(Product product);
+        IDataResult <List<Product>> GetAll();
+        IDataResult<Product> GetById(int productId);
+        IDataResult<List<Product>> GetAllByCategoryId(int Id);
+        IDataResult<List<ProductDetailDto>> GetProductDetailDtos();
+        IDataResult<List<Product>> GetByUnitPrice(decimal min , decimal max);
+     
+     
+      
     }
 }
