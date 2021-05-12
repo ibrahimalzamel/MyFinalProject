7078fe183077079d91b57e97d03c1042ddfe0916
İBRAHİM ELZAMİL <ibooalzam@gmail.com>
2021-05-12 16:26:06 +0300

    son commit 3

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -31,7 +31,7 @@ namespace Business.Concrete
 
         //salting
         //Claim
-     //  [SecuredOperation("product.add,admin")]
+        [SecuredOperation("product.add,admin")]
         [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
